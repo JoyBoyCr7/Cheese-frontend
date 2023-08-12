84c4b917504997cabfc7deb7e1cc312d9bd78ef9
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-08-12 17:55:09 -0500

    input field updates

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,16 +6,25 @@ import { useState } from "react"
 
 function Index(props){
     const cheeses = useLoaderData()
+    const [completed, setcompleted] = useState(true)
+    function changeState(event){
+        if (event.target.value.trim() === ""){
+            setcompleted(!completed)
+            event.target.value = ""
+            alert(`Please input a ${event.target.placeholder}`)
+        }
+        
+    }
 
     return (
         <div className="main">
             <h2 className="header">Create a new Cheese Below</h2>
             <Form action="/create" method="post" className="form">
                 <div className="inputs">
-                    <input required type="text" name="name" placeholder="cheese name" />
-                    <input type="text" name="countryOfOrigin" placeholder="Origin"/>
-                    <input type="text" name="image" placeholder="cheese image" required/>
-                    <input type="submit" value="Create Cheese"/>
+                    <input required type="text" name="name" placeholder="cheese name" onChange={changeState}/>
+                    <input required type="text" name="countryOfOrigin" placeholder="Origin" onChange={changeState}/>
+                    <input required type="text" name="image" placeholder="cheese image" onChange={changeState}/>
+                    <input type="submit" value="Create Cheese" />
                 </div>
             </Form>
             <div className="allcheese">
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,9 +1,24 @@
 import { useLoaderData } from "react-router-dom"
 import { Form } from "react-router-dom"
+import { useState } from "react"
+
 
 function Show(props){
     const cheese = useLoaderData()
+    /// testing
     console.log(cheese)
+    //  testing
+
+    const [completed, setcompleted] = useState(true)
+    function changeState(event){
+        if (event.target.value.trim() === ""){
+            setcompleted(!completed)
+            event.target.value = ""
+            alert(`Please input a ${event.target.placeholder}`)
+        }
+        
+    }
+
     return <div>
             <h1>{cheese.name}</h1>
             <img src={cheese.image} alt={cheese.name}/>
@@ -11,9 +26,9 @@ function Show(props){
 
             <h2>Update {cheese.name}</h2>
             <Form action={`/update/${cheese._id}`} method="put">
-                <input required type="text" name="name" placeholder="cheese name"/>
-                <input type="text" name="countryOfOrigin" placeholder="Origin"/>
-                <input type="text" name="image" placeholder="cheese image"/>
+                <input required type="text" name="name" placeholder="cheese name" onChange={changeState}/>
+                <input required type="text" name="countryOfOrigin" placeholder="Origin" onChange={changeState}/>
+                <input required type="text" name="image" placeholder="cheese image" onChange={changeState}/>
                 <input type="submit" value="update Cheese"/>
             </Form>
             <h2>Delete {cheese.name}</h2>
